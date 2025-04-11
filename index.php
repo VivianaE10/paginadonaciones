318852cc025b57b6e1da8d0a42ba74a91a065c73
Sergio Garcia <sergiogarciauribe612@gmail.com>
2025-04-10 19:36:01 -0500

    cambiando las rutas de los archivos

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
-<bod>
+  <bod>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-<div class="container-fluid mt-5">
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-  <a class="navbar-brand" style="color: #4A90E2;">Donaciones.com</a>
-    <form class="d-flex" role="search" >
-      <a href="./FormRegistro/index.php" class="btn me-3"  style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Registro</a>
-      <button class="btn"  style="--bs-btn-bg: #0056D2; color: #4A90E2;"type="submit">Iniciar Sesion</button>
-    </form>
-  </div>
-  <div class="container-fluid p-0" >
-  <img src="https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=600" class="img-fluid w-100" alt="imagen">
-  </div>
-</div>
-<h2>siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
-</div>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <div class="container-fluid mt-5">
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" style="color: #4A90E2;">Donaciones.com</a>
+          <form class="d-flex" role="search">
+            <a href="./Form/RegistroUsuario/registroUsuario.html" class="btn me-3" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Registro</a>
+            <button class="btn" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Iniciar Sesion</button>
+          </form>
+        </div>
+        <div class="container-fluid p-0">
+          <img src="https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=600" class="img-fluid w-100" alt="imagen">
+        </div>
+    </div>
+    <h2>siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
+    </div>
+    </body>
+
 </html>
