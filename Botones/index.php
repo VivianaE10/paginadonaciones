9eeedb3640705afc310adfb524a7e47554bb7402
Viviana <vivioescobar@gmail.com>
2025-04-14 13:31:43 -0500

    Agregué el ícono y los estilos al formulario de registro de donaciones

--- Botones/index.php
+++ Botones/index.php
@@ -14,7 +14,7 @@
       <div class="card-body">
       <h5 class="card-title">Special title treatment</h5>
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="/paginadonaciones/Form/RegistroDonaciones/index.php" class="btn btn-primary">Registro Donaciones</a>
+      <a href="/paginadonaciones/Form/RegistroDonaciones/registroDonaciones.html" class="btn btn-primary">Registro Donaciones</a>
       </div>
     </div>
   </div>
