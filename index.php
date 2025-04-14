e3dbf6a826ae279532eb379aea683b939f6f5894
Sergio Garcia <sergiogarciauribe612@gmail.com>
2025-04-14 17:19:38 -0500

    probando rutas

--- index.php
+++ index.php
@@ -1,46 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
-<<<<<<< HEAD
-  <bod>
-    <div class="container-fluid mt-5">
-      <nav class="navbar bg-body-tertiary">
-        <a class="navbar-brand" style="color: #4A90E2;">Donaciones.com</a>
-        <div class="container-fluid">
-          <form class="d-flex" role="search">
-            <a href="/Form/RegistroUsuario/registroUsuario.html" class="btn me-3" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Registro</a>
-            <a href="/FormLogin/index.php" class="btn" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Iniciar Sesion</a>
-          </form>
-        </div>
-        <div class="container-fluid p-0">
-          <img src="https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=600" class="img-fluid w-100" alt="imagen">
-        </div>
-    </div>
-    <h2>siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
-=======
-<bod>
-<div class="container-fluid mt-5">
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <form class="d-flex" role="search">
-            <a href="./Form/RegistroUsuario/registroUsuario.html" class="btn me-3" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Registro</a>
-            <a href="./FormLogin/index.php" class="btn" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Iniciar Sesion</a>
-    </form>
+<body>
+  <div class="container-fluid mt-5">
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <form class="d-flex" role="search">
+          <a href="./Form/RegistroUsuario/registroUsuario.html" class="btn me-3" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Registro</a>
+          <a href="./FormLogin/index.php" class="btn" style="--bs-btn-bg: #0056D2; color: #4A90E2;" type="submit">Iniciar Sesion</a>
+        </form>
+      </div>
+      <div class="container-fluid p-0">
+        <img src="https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=600" class="img-fluid w-100" alt="imagen">
+      </div>
   </div>
-  <div class="container-fluid p-0" >
-  <img src="https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=600" class="img-fluid w-100" alt="imagen">
+  <h2>siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
   </div>
-</div>
-<h2>siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
-</div>
->>>>>>> 5a0d229cfe6875a7fff9f3e8892b7d3e62af0604
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
