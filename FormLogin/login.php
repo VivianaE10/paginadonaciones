1eeb16f082244e8d2b0ab9adc547a5c99d33287a
Sergio Garcia <sergiogarciauribe612@gmail.com>
2025-04-14 17:46:44 -0500

    redirecionado el boton login con PHP

--- FormLogin/login.php
+++ FormLogin/login.php
@@ -1,3 +1,15 @@
 <?php
 
+
 require_once '../Form/RegistroUsuario/registroUsuario.php';
+function redirectWelcome()
+{
+  header("location: ../Botones/index.php");
+  exit();
+}
+
+if ($conexion->connect_error) {
+  echo ('Error de conexion');
+} else {
+  redirectWelcome();
+}
