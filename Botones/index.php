52e74180521695e67c6b8f0dbb6db820294cd305
daniel arias <alejo66rocha@gmail.com>
2025-04-15 20:30:13 -0500

    conexion reclamos

--- Botones/index.php
+++ Botones/index.php
@@ -23,7 +23,7 @@
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">PQRS</a>
+        <a href="/paginadonaciones/Form/formReclamos/reclamos.html" class="btn btn-primary">PQRS</a>
       </div>
     </div>
   </div>
