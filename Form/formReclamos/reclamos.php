91255aaa36d7dcef2a4b6e91b5c14ad4ebf9a371
Sergio Garcia <sergiogarciauribe612@gmail.com>
2025-04-21 06:47:23 -0500

    haciendo cambios en donaciones y en reclamos

--- Form/formReclamos/reclamos.php
+++ Form/formReclamos/reclamos.php
@@ -1,11 +1,80 @@
-<!DOCTYPE html>
+<?php
+
+require_once "../../database/MySQLi/Conexion.php";
+
+//funcion de php para capturar errores
+session_start();
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+
+$conexion = CreateConnection();
+
+if ($conexion->connect_error) {
+
+  error_log("Error de conexión de la base de datos: " . $conexion->connect_error);
+  echo ("Error en la conexión. intentalo más tarde");
+  exit();
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  //Obtener los datos enviados desde el formulario de registroUsuario
+  //Usamos  null coalescing operator (??) para evitar warnings si no existen
+  $id_usuario = $_SESSION['usuarioID']; // Aquí recuperas el ID del login
+  $fullName  = cleanInput($_POST['nombre'] ?? '');
+  $emailUser = cleanInput($_POST['email'] ?? '');
+  $dateReclamo = cleanInput($_POST['fechaInconveniente'] ?? '');
+  $messageReclamo = cleanInput($_POST['message'] ?? '');
+
+
+  //Validación de campos vacíos
+
+  if (empty($fullName) || empty($emailUser) || empty($dateReclamo) || empty($messageReclamo)) {
+    echo ("Todos los campos son obligatorios");
+    exit();
+  }
+
+  //preparar consulta en SQL
+
+  $sql = "INSERT INTO PQR ()"
+}
+
+
+function cleanInput($input)
+{
+  $input = trim($input); // Elimina espacios en blanco al inicio y final
+  // Codifica caracteres especiales HTML para prevenir XSS si se imprime directamente en HTML
+  $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+  return $input;
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Reclamos</title>
-  <!-- Agregar los enlaces a los estilos de Bootstrap -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <title>Reclamos</title> -->
+<!-- Agregar los enlaces a los estilos de Bootstrap -->
+<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
   <div class="container mt-5">
@@ -26,11 +95,12 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
-  <!-- Agregar el enlace al script de Bootstrap (requerido para algunos componentes interactivos) -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Agregar el enlace al script de Bootstrap (requerido para algunos componentes interactivos) -->
+<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+</html>-->
+
 
 <!--//' OR '1'='1-->
--- js/validacionReclamos.js
+++ js/validacionReclamos.js
@@ -6,32 +6,32 @@ function limpiarCorreo(correo) {
   return correo.trim().replace(/['\s]/g, "".trim());
 }
 document
-  .getElementById("reclamos")
+  .getElementById("reclamosForm")
   .addEventListener("submit", function (e) {
     e.preventDefault(); // preventDefault evita que el formulario se envíe inmediatamente, para poder hacer validaciones primero.
 
-    //Odtener y limpiar datos 
+    //Odtener y limpiar datos
     //.trim() para eliminar espacios al principio y final de la cadena.
     const nameFull = limpiarCadena(
-      document.getElementById("nameFull").value.trim()
+      document.getElementById("nombre").value.trim()
     );
     const emailReclamo = limpiarCadena(
-      document.getElementById("dateDonation").value.trim()
+      document.getElementById("email").value.trim()
     );
     const dateReclamo = limpiarCadena(
-      document.getElementById("holderName").value.trim()
+      document.getElementById("fecha").value.trim()
     );
     const noteReclamo = limpiarCadena(
-      document.getElementById("cardNumber").value.trim()
+      document.getElementById("message").value.trim()
     );
 
     //validar que los campos no esten vacios
     if (
-     nameFull.length === 0 ||
-     emailReclamo.length === 0 ||
-     dateReclamo.length === 0 ||
-     noteReclamo.length === 0 ||
-    ){
+      nameFull.length === 0 ||
+      emailReclamo.length === 0 ||
+      dateReclamo.length === 0 ||
+      noteReclamo.length === 0
+    ) {
       alert("Todos los campos son obligatorios");
       return;
     } else {
@@ -41,7 +41,6 @@ document
     // Si todo está correcto, enviar el formulario
     alert("Formulario validado correctamente. Enviando datos...");
     console.log("Formulario validado correctamente. Enviando datos...");
-    document.getElementById("reclamos").submit();
   });
 
-  //valida un el formulario de donaciones en el navegador antes de que se envíe. Sirve para asegurar que todos los campos estén llenos y limpios antes de enviar los datos al servidor.
+//valida un el formulario de donaciones en el navegador antes de que se envíe. Sirve para asegurar que todos los campos estén llenos y limpios antes de enviar los datos al servidor.
