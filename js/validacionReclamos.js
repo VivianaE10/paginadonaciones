60bc554571c4f399bc910a849a72263ff1ecfb24
Sergio Garcia <sergiogarciauribe612@gmail.com>
2025-04-21 23:12:12 -0500

    Revert "Se hace formulario de PQRs enviando datos a MySql"
    This reverts commit f69385713544e1ca8f339b5f51a8347aaa6998d8.

--- js/validacionReclamos.js
+++ js/validacionReclamos.js
@@ -2,41 +2,44 @@ function limpiarCadena(cadena) {
   return cadena.replace(/['@\s]/g, "").trim(); //Reemplaza caracteres especiales en el formulario por un string vacio
 }
 
-document
-  .getElementById("reclamosForm")
-  .addEventListener("submit", function (e) {
-    e.preventDefault(); // preventDefault evita que el formulario se envíe inmediatamente, para poder hacer validaciones primero.
+function limpiarCorreo(correo) {
+  return correo.trim().replace(/['\s]/g, "".trim());
+}
+document.getElementById("reclamos").addEventListener("submit", function (e) {
+  e.preventDefault(); // preventDefault evita que el formulario se envíe inmediatamente, para poder hacer validaciones primero.
 
-    //Odtener y limpiar datos
-    //.trim() para eliminar espacios al principio y final de la cadena.
+  //Odtener y limpiar datos
+  //.trim() para eliminar espacios al principio y final de la cadena.
+  const nameFull = limpiarCadena(
+    document.getElementById("nameFull").value.trim()
+  );
+  const emailReclamo = limpiarCadena(
+    document.getElementById("dateDonation").value.trim()
+  );
+  const dateReclamo = limpiarCadena(
+    document.getElementById("holderName").value.trim()
+  );
+  const noteReclamo = limpiarCadena(
+    document.getElementById("cardNumber").value.trim()
+  );
 
-    const tipoPeticion = limpiarCadena(
-      document.getElementById("tipoReclamo").value.trim()
-    );
+  //validar que los campos no esten vacios
+  if (
+    nameFull.length === 0 ||
+    emailReclamo.length === 0 ||
+    dateReclamo.length === 0 ||
+    noteReclamo.length === 0
+  ) {
+    alert("Todos los campos son obligatorios");
+    return;
+  } else {
+    console.log("Todos los datos estan llenos");
+  }
 
-    const dateReclamo = limpiarCadena(
-      document.getElementById("fecha").value.trim()
-    );
-    const noteReclamo = limpiarCadena(
-      document.getElementById("message").value.trim()
-    );
-
-    //validar que los campos no esten vacios
-    if (
-      tipoPeticion.length === 0 ||
-      dateReclamo.length === 0 ||
-      noteReclamo.length === 0
-    ) {
-      alert("Todos los campos son obligatorios");
-      return;
-    } else {
-      console.log("Todos los datos estan llenos");
-    }
-
-    // Si todo está correcto, enviar el formulario
-    alert("Formulario validado correctamente. Enviando datos...");
-    console.log("Formulario validado correctamente. Enviando datos...");
-    document.getElementById("reclamos").submit();
-  });
+  // Si todo está correcto, enviar el formulario
+  alert("Formulario validado correctamente. Enviando datos...");
+  console.log("Formulario validado correctamente. Enviando datos...");
+  document.getElementById("reclamos").submit();
+});
 
 //valida un el formulario de donaciones en el navegador antes de que se envíe. Sirve para asegurar que todos los campos estén llenos y limpios antes de enviar los datos al servidor.
