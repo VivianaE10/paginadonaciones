aa9dfd9c0e891ff20b8f4816b2da93ad08c2737e
Viviana <vivioescobar@gmail.com>
2025-04-22 15:39:10 -0500

    Se le agrego el diseño TOP NAV a todos los formularios

--- Botones/index.php
+++ Botones/index.php
@@ -6,9 +6,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
+  <div class="top-nav" id="home">
+   <div class="container">
+    <div class="row justify-content-between">
+      <div class="col-auto">
+        <p> <i class='bx bxs-envelope'></i> donaciones@gmail.com</p>
+        <p> <i class='bx bxs-phone-call'></i> 123 456-7890</p>
+      </div>
+      <div class="col-auto social-icons">
+        <a href="#"><i class='bx bxl-facebook'></i></a>
+        <a href="#"><i class='bx bxl-twitter'></i></a>
+        <a href="#"><i class='bx bxl-instagram'></i></a>
+        <a href="#"><i class='bx bxl-pinterest'></i></a>
+      </div>
+      </div>
+    </div>
+  </div>
   <div class="container mt-5"> 
     <div class="row">
       <div class="col-sm-6 mb-3 mb-sm-0">
@@ -16,7 +34,7 @@
           <div class="card-body">
             <h5 class="card-title">Registro Donaciones🩵</h5>
             <p class="card-text">"Tu ayuda puede cambiar vidas. Gracias por tu apoyo y solidaridad."</p>
-            <a href="../Form/RegistroDonaciones/registroDonaciones.html" class="btn btn-primary">Donar</a>
+            <a href="../Form/RegistroDonaciones/registroDonaciones.html" class="btn btn-primary" style="color: white">Donar</a>
           </div>
         </div>
       </div>
@@ -25,7 +43,7 @@
           <div class="card-body">
             <h5 class="card-title">Special title treatment</h5>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="../Form/formReclamos/reclamos.html" class="btn btn-primary">PQRS</a>
+            <a href="../Form/formReclamos/reclamos.html" class="btn btn-primary" style="color: white">PQRS</a>
           </div>
         </div>
       </div>
