1bab4352a313d9d70fef5e008463e755f78f8dfd
Sergio Garcia <sergiogarciauribe612@gmail.com>
2025-05-24 06:38:21 -0500

    agregado enums para el formulario de registro de usuarios

--- Enums/error_registro.php
+++ Enums/error_registro.php
@@ -3,4 +3,7 @@
 enum errorRegistro: string
 {
   case camposVacios = 'Todos los campos son obligatorios';
+  case ContrasenaNoCoincide = "Las contraseñas no coinciden.";
+  case ContrasenaEspacios = "La contraseña no debe contener espacios.";
+  case CorreoDuplicado = "El correo electrónico ya está registrado.";
 }
